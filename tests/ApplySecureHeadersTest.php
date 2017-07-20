ee03ed78845ae297b14f6a9d6315051e411a8b96
Mike Francis <mikeffrancis@gmail.com>
2017-07-20 19:38:50 +0100

    Travis and tests (#3)
    * Added basic test
    
    * Added Travis CI
    
    * Wrong version syntax
    
    * Still the wrong syntax!
    
    * Fix up PHP versions

---
+++ tests/ApplySecureHeadersTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace MikeFrancis\LaravelSecureHeaders\Tests;
+
+use Aidantwoods\SecureHeaders\SecureHeaders;
+use Illuminate\Contracts\Config\Repository;
+use Illuminate\Http\Request;
+use MikeFrancis\LaravelSecureHeaders\ApplySecureHeaders;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class ApplySecureHeadersTest extends TestCase
+{
+    /**
+     * Ensure that the middleware adds the appropriate headers.
+     *
+     * @return void
+     */
+    public function testMiddlewareAddsAppropriateHeaders()
+    {
+        $config = $this->createMock(Repository::class);
+        $request = new Request();
+        $response = new Response();
+        $secureHeaders = new SecureHeaders();
+
+        $middleware = new ApplySecureHeaders($config, $secureHeaders);
+
+        $result = $middleware->handle($request, function ($foo) use ($response) {
+            return $response;
+        });
+
+        $headers = $result->headers->all();
+
+        $this->assertArrayHasKey('x-xss-protection', $headers);
+        $this->assertArrayHasKey('x-frame-options', $headers);
+    }
+}
