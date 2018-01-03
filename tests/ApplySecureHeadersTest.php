8b30bbc1ade1780fc425c85f82589f2bb863f394
Aidan Woods <aidantwoods@gmail.com>
2018-01-03 00:34:27 +0000

    Refactor tests into reusable stages

--- tests/ApplySecureHeadersTest.php
+++ tests/ApplySecureHeadersTest.php
@@ -12,87 +12,37 @@ use Symfony\Component\HttpFoundation\Response;
 class ApplySecureHeadersTest extends TestCase
 {
     /**
-     * Ensure that the middleware adds the default headers.
+     * Ensure that the middleware adds the base headers.
      *
      * @return void
      */
-    public function testMiddlewareAddsDefaultHeaders()
+    public function testMiddlewareAddsBaseHeaders()
+    {
+        $result = $this->applySecureHeadersWithConfig(new Response, []);
+        $this->assertBaseHeadersPresent($result->headers->all());
+    }
+
+    /**
+     * Ensure that safe-mode neuters hsts.
+     *
+     * @return void
+     */
+    public function testHstsAndSafeMode()
     {
         $response = $this->applySecureHeadersWithConfig(new Response());
         $headers = $response->headers->all();
         // configuration
-        $map = [
-            ['secure-headers.csp', [], ['csp' => []]],
+        $configMap = [
             ['secure-headers.hsts.enabled', false, true],
             ['secure-headers.safeMode', false, true],
         ];
 
-        $config = $this->createMock(Repository::class);
-        $config->method('get')->will($this->returnValueMap($map));
-        // return default (second arg) if not in configuration
-        $config->method('get')->will($this->returnArgument(1));
-
-        $this->assertArrayHasKey('x-xss-protection', $headers);
-        $this->assertArrayHasKey('x-frame-options', $headers);
-    }
-
-    /**
-     * Ensure that the middleware enables safe mode.
-     *
-     * @return void
-     */
-    public function testMiddlewareAddsHSTSHeaders()
-    {
-        $config = [
-            'hsts' => [
-                'enabled' => true,
-            ],
-        ];
-
-        $response = $this->applySecureHeadersWithConfig(new Response(), 'hsts.enabled', $config);
-        $headers = $response->headers->all();
+        $result  = $this->applySecureHeadersWithConfig(new Response, $configMap);
+        $headers = $result->headers->all();
 
         $this->assertArrayHasKey('strict-transport-security', $headers);
-    }
-
-    /**
-     * Ensure that the middleware enables safe mode.
-     *
-     * @return void
-     */
-    public function testMiddlewareEnablesSafeMode()
-    {
-        $config = [
-            'hsts' => [
-                'enabled' => true,
-            ],
-            'safeMode' => true,
-        ];
-
-        $response1 = $this->applySecureHeadersWithConfig(new Response(), 'hsts.enabled', $config);
-        $response2 = $this->applySecureHeadersWithConfig($response1, 'safeMode', $config);
-        $headers = $response2->headers->all();
-
-        $this->assertNotEquals('max-age=31536000', $headers['strict-transport-security'][0]);
-    }
-
-    /**
-     * Ensure that the middleware adds the CSP headers.
-     *
-     * @return void
-     */
-    public function testMiddlewareAddsCSPHeaders()
-    {
-        $config = [
-            'csp' => [
-                'default' => 'self',
-            ],
-        ];
-
-        $response = $this->applySecureHeadersWithConfig(new Response(), 'csp', $config);
-        $headers = $response->headers->all();
-
-        $this->assertArrayHasKey('content-security-policy', $headers);
+        $this->assertSame($headers['strict-transport-security'][0], 'max-age=86400');
+        $this->assertBaseHeadersPresent($headers);
     }
 
     /**
@@ -114,35 +64,46 @@ class ApplySecureHeadersTest extends TestCase
     }
 
     /**
+     * Assert base headers are present given an array of headers.
+     *
+     * @param array<string, string[]> $headers
+     * @return void
+     */
+    private function assertBaseHeadersPresent(array $headers)
+    {
+        $this->assertArrayHasKey('x-permitted-cross-domain-policies', $headers);
+        $this->assertArrayHasKey('x-content-type-options', $headers);
+        $this->assertArrayHasKey('expect-ct', $headers);
+        $this->assertArrayHasKey('referrer-policy', $headers);
+        $this->assertArrayHasKey('x-xss-protection', $headers);
+        $this->assertArrayHasKey('x-frame-options', $headers);
+    }
+
+    /**
      * Apply SecureHeaders from the given config to a Response.
      *
      * @param Response $response
-     * @param string|null $key
-     * @param array|null $configMap
+     * @param ?array $configMap
      * @return Response
      */
-    private function applySecureHeadersWithConfig(Response $response, string $key = null, array $configMap = null)
-    {
-        /** @var Repository|\PHPUnit_Framework_MockObject_MockObject $config */
+    private function applySecureHeadersWithConfig(
+        Response $response,
+        ?array $configMap = null
+    ) {
         $config = $this->createMock(Repository::class);
+        if (isset($configMap)) {
+            $config->method('get')->will($this->returnValueMap($configMap));
+        }
+        // return default (second arg) if not in configuration
+        $config->method('get')->will($this->returnArgument(1));
 
-        $config->expects($this->any())
-            ->method('get')
-            ->with($this->anything())
-            ->will($this->returnCallback(function () use ($key, $configMap) {
-                $args = func_get_args();
-
-                if ($args[0] === "secure-headers.{$key}") {
-                    return $configMap;
-                }
-
-                return $args[1];
-            }));
-
-        $secureHeaders = new SecureHeaders();
+        $secureHeaders = new SecureHeaders;
         $secureHeaders->errorReporting(false);
-        $middleware = new ApplySecureHeaders($config, $secureHeaders);
+        $middleware    = new ApplySecureHeaders($config, $secureHeaders);
 
-        return $middleware->handle(new Request, function () use ($response) { return $response; });
+        return $middleware->handle(
+            new Request,
+            function () use ($response) { return $response; }
+        );
     }
 }
