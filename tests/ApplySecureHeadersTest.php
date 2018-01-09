fdb5e72cefc4684fae30780b3c701a05089b826b
Mike Francis <mikeffrancis@gmail.com>
2018-01-09 10:52:10 +0000

    Fix expect-ct headers (#19)

--- tests/ApplySecureHeadersTest.php
+++ tests/ApplySecureHeadersTest.php
@@ -12,33 +12,86 @@ use Symfony\Component\HttpFoundation\Response;
 class ApplySecureHeadersTest extends TestCase
 {
     /**
-     * Ensure that the middleware adds the appropriate headers.
+     * Ensure that the middleware adds the default headers.
      *
      * @return void
      */
-    public function testMiddlewareAddsAppropriateHeaders()
+    public function testMiddlewareAddsDefaultHeaders()
     {
-        $config = $this->createMock(Repository::class);
-        $config->method('get')->willReturn(['csp' => []]);
-        $config->method('get')->willReturn(true); // hsts
-        $config->method('get')->willReturn(true); // safeMode
-
-        $request = new Request();
-
-        $response = new Response();
-        $response->headers->set('set-cookie', 'someCookieToIgnore');
-
-        $secureHeaders = new SecureHeaders();
-
-        $middleware = new ApplySecureHeaders($config, $secureHeaders);
-
-        $result = $middleware->handle($request, function ($foo) use ($response) {
-            return $response;
-        });
-
-        $headers = $result->headers->all();
+        $response = $this->applySecureHeadersWithConfig(new Response());
+        $headers = $response->headers->all();
 
         $this->assertArrayHasKey('x-xss-protection', $headers);
         $this->assertArrayHasKey('x-frame-options', $headers);
+    }
+
+    /**
+     * Ensure that the middleware adds the CSP headers.
+     *
+     * @return void
+     */
+    public function testMiddlewareAddsCSPHeaders()
+    {
+        $config = [
+            'csp' => [
+                'default' => 'self',
+            ],
+        ];
+
+        $response = $this->applySecureHeadersWithConfig(new Response(), 'csp', $config);
+        $headers = $response->headers->all();
+
+        $this->assertArrayHasKey('content-security-policy', $headers);
+    }
+
+    /**
+     * Ensure that the middleware adds the expect-ct headers.
+     *
+     * @return void
+     */
+    public function testMiddlewareAddsExpectCTHeaders()
+    {
+        $time = time();
+        $config = [
+            'maxAge' => $time,
+        ];
+
+        $response = $this->applySecureHeadersWithConfig(new Response(), 'expectCT', $config);
+        $headers = $response->headers->all();
+
+        $this->assertEquals("max-age={$time}", $headers['expect-ct'][0]);
+    }
+
+    /**
+     * Apply SecureHeaders from the given config to a Response.
+     *
+     * @param Response $response
+     * @param string|null $key
+     * @param array|null $configMap
+     * @return Response
+     */
+    private function applySecureHeadersWithConfig(Response $response, string $key = null, array $configMap = null)
+    {
+        /** @var Repository|\PHPUnit_Framework_MockObject_MockObject $config */
+        $config = $this->createMock(Repository::class);
+
+        $config->expects($this->any())
+            ->method('get')
+            ->with($this->anything())
+            ->will($this->returnCallback(function () use ($key, $configMap) {
+                $args = func_get_args();
+
+                if ($args[0] === "secure-headers.{$key}") {
+                    return $configMap;
+                }
+
+                return $args[1];
+            }));
+
+        $secureHeaders = new SecureHeaders();
+        $secureHeaders->errorReporting(false);
+        $middleware = new ApplySecureHeaders($config, $secureHeaders);
+
+        return $middleware->handle(new Request, function () use ($response) { return $response; });
     }
 }
